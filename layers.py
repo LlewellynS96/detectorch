3e5a7004736e3abf02bb1ede88fde97eb690863e
LlewellynS96 <llewellyn121@gmail.com>
2019-09-27 14:08:18 +0200

    Cleaned up.

--- layers.py
+++ layers.py
@@ -1,16 +1,6 @@
 import torch
-import torchvision
-import time
-import os
-import copy
-import torchsummary
-import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-import numpy as np
-from torchvision import datasets, models, transforms
-import matplotlib.pyplot as plt
 
 
 class FocalLoss(nn.Module):
